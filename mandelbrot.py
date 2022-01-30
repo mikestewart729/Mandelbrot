3f8b25f87b386ccea133d4ea5f8af1432d01d588
Mike <mikestewart729@gmail.com>
2022-01-30 09:45:38 -0500

    Refactor code for easier plotting of different styles

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,5 +1,7 @@
 # Mandelbrot.py
 
+from math import log, log2
+
 MAX_ITER = 80 # Maximum number of times to iterate in computing set
 
 def mandelbrot(c):
@@ -22,6 +24,30 @@ def mandelbrot(c):
         z = z * z + c 
         n += 1
     return n
+
+def mandelbrot_smooth(c):
+    """
+    Returns the fractional escape count or normalized iteration count of an 
+    initial complex number c
+
+    Args:
+       c (complex): Point in complex space to test for membership in
+          Mandelbrot set.
+    
+    Returns:
+       int: number of iterations to reach modulus greater than 2, or else
+          MAX_ITER if more iterations would be needed
+    """
+    z = 0
+    n = 0
+    while abs(z) <= 2 and n < MAX_ITER:
+        z = z * z + c 
+        n += 1
+
+    if n == MAX_ITER: 
+        return MAX_ITER
+    
+    return n + 1 - log(log2(abs(z)))
 
 if __name__ == "__main__":
     for x in range(-10, 10, 5):
